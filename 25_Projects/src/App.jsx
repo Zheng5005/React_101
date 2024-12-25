aae89df4189d0ffd739878e05b4e8defab4f56e7
Zheng5005 <u.gomezherrera@gmail.com>
2024-12-25 15:03:37 -0600

    Tab Component

--- 25_Projects/src/App.jsx
+++ 25_Projects/src/App.jsx
@@ -11,13 +11,14 @@ import sideMenu from './components/tree_view/data.js'
 import QRCodeGenerator from './components/qrcode/QRCode.jsx'
 import ThemeSwitch from './components/theme_switch/ThemeSwitch.jsx'
 import Scroll from './components/scrolls/scroll_indicator/Scroll.jsx'
+import TabTest from './components/tabs/tab-test.jsx'
 
 function App() {
  return (
     <>
       <Accordion />
       <RandomColor />
-      <StarRating noOfStars={10}/> {/* npm i react-icons -> something like that*/}
+      <StarRating noOfStars={10}/> {/* npm i react-icons*/}
 
       {/* The image slide kinda works, but it doesn't show any images, but you can slide through for some reason */}
       <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"}/>
@@ -30,6 +31,7 @@ function App() {
       <ThemeSwitch />
 
       <Scroll url={'https://dummyjson.com/products?limit=50'}/>
+      <TabTest />
     </>
   )
 }
